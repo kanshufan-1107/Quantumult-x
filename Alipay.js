1fa83df23dc4f4c2837dda6fba7c170118c294b7
看书范 <1536576901@qq.com>
2022-09-27 11:39:45 +0800

    修改并重命名

--- Alipay.js
+++ Alipay.js
@@ -3,10 +3,9 @@
 */
 
 const $ = new compatibility()
-const wait_family = $.read('Alipay_wait_family') * 1000 || 5000
-const wait_mayi = $.read('Alipay_wait_mayi') * 1000 || 5000
+const wait_family = 5000
+const wait_mayi = 5000
 const point = "alipays://platformapi/startapp?appId=20000160&url=/www/pointSignIn.html"
-const familypoint = "alipays://platformapi/startapp?appId=2019052365379124"
 const mayi = "alipay://platformapi/startapp?appId=60000002"
 
 let delay = function(s){
@@ -18,9 +17,6 @@ let delay = function(s){
 delay().then(function(){
     $.notify("支付宝", "", "领积分啦", point)
     return delay(wait_family)
-}).then(function(){
-    $.notify("支付宝", "", "领家庭积分啦", familypoint)
-    return delay(wait_mayi)
 }).then(function(){
     $.notify("支付宝", "", "收能量啦", mayi)
 }).finally(() => $done())
